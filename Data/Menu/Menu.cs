5e4e32177f124da8c9967dbcd35ab1b713e20b27
Mason Potter <masonpotter1@ksu.edu>
2020-09-13 20:05:12 -0500

    Milestone 3

---
+++ Data/Menu/Menu.cs
@@ -0,0 +1,121 @@
+﻿using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BleakwindBuffet.Data.Menu
+{
+    /// <summary>
+    /// this is a class that represnets a menu item
+    /// </summary>
+    public static class Menu
+    {
+        /// <summary>
+        /// adds all entrees to the menu
+        /// </summary>
+        /// <returns> returns a list of all entrees</returns>
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entreeList = new List<IOrderItem>();
+            BriarheartBurger bb = new BriarheartBurger();
+            DoubleDraugr dd = new DoubleDraugr();
+            GardenOrcOmelette gc = new GardenOrcOmelette();
+            PhillyPoacher ph = new PhillyPoacher();
+            SmokehouseSkeleton sh = new SmokehouseSkeleton();
+            ThalmorTriple tt = new ThalmorTriple();
+            ThugsTBone tb = new ThugsTBone();
+            entreeList.Add(bb);
+            entreeList.Add(dd);
+            entreeList.Add(gc);
+            entreeList.Add(ph);
+            entreeList.Add(sh);
+            entreeList.Add(tt);
+            entreeList.Add(tb);
+
+            return (entreeList);
+        }
+        /// <summary>
+        /// adds all drinks to the menu
+        /// </summary>
+        /// <returns>returns a list of all drinks </returns>
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            List<IOrderItem> Drinklist = new List<IOrderItem>();
+
+            foreach(Size s in Enum.GetValues(typeof(Size)))
+            {
+                AretinoAppleJuice aj = new AretinoAppleJuice();
+                aj.Size = s;
+                Drinklist.Add(aj);
+
+                foreach(SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
+                {
+                    SailorSoda ss = new SailorSoda();
+                    ss.Size = s;
+                    ss.flavor = f;
+                    Drinklist.Add(ss);
+                }
+                CandlehearthCoffee cf = new CandlehearthCoffee();
+                cf.Size = s;
+                Drinklist.Add(cf);
+                CandlehearthCoffee dcf = new CandlehearthCoffee();
+                dcf.Size = s;
+                dcf.decaf = true;
+                Drinklist.Add(dcf);
+
+                MarkarthMilk mm = new MarkarthMilk();
+                mm.Size = s;
+                Drinklist.Add(mm);
+
+                WarriorWater ww = new WarriorWater();
+                ww.Size = s;
+                Drinklist.Add(ww);
+
+            }
+            return Drinklist;
+        }
+        /// <summary>
+        /// adds all sides to the list
+        /// </summary>
+        /// <returns>returns a list of all sides</returns>
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            List<IOrderItem> Sidelist = new List<IOrderItem>();
+
+            foreach(Size s in Enum.GetValues(typeof(Size)))
+            {
+                DragonbornWaffleFries df = new DragonbornWaffleFries();
+                df.Size = s;
+                FriedMiraak fm = new FriedMiraak();
+                fm.Size = s;
+                MadOtarGrits mo = new MadOtarGrits();
+                mo.Size = s;
+                VokunSalad vs = new VokunSalad();
+                vs.Size = s;
+
+                Sidelist.Add(df);
+                Sidelist.Add(fm);
+                Sidelist.Add(mo);
+                Sidelist.Add(vs);
+            }
+            return Sidelist;
+
+        }
+
+        public static IEnumerable<IOrderItem> FullMenue()
+        {
+            List<IOrderItem> menuItems = new List<IOrderItem>();
+            menuItems.AddRange(Entrees());
+            menuItems.AddRange(Drinks());
+            menuItems.AddRange(Sides());
+
+            return menuItems;
+
+        }
+    }
+}
---
+++ Test_Types/MenuTest.cs
@@ -0,0 +1,95 @@
+﻿using BleakwindBuffet.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Menu;
+using Data;
+
+namespace Test_Types
+{
+    public class MenuTest
+    {
+        /// <summary>
+        /// makes sure that all of the entrees apear on the menue 
+        /// </summary>
+        [Fact]
+        public void MenuReturnsAllEntrees()
+        {
+            Assert.Contains(Menu.Entrees(), (item) => { return item is BriarheartBurger; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is DoubleDraugr; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is GardenOrcOmelette; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is PhillyPoacher; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is SmokehouseSkeleton; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is ThalmorTriple; });
+            Assert.Contains(Menu.Entrees(), (item) => { return item is ThugsTBone; });
+        }
+        /// <summary>
+        /// makes sure that all sides are on the menu
+        /// </summary>
+        [Fact]
+        public void MenuReturnsAllSides()
+        {
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Small Dragonborn Waffle Fries"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Small Fried Miraak"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Small Mad Otar Grits"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Small Vokun Salad"); });
+
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Medium Dragonborn Waffle Fries"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Medium Fried Miraak"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Medium Mad Otar Grits"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Medium Vokun Salad"); });
+
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Large Dragonborn Waffle Fries"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Large Fried Miraak"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Large Mad Otar Grits"); });
+            Assert.Contains(Menu.Sides(), (item) => { return item.ToString().Equals("Large Vokun Salad"); });
+        }
+        /// <summary>
+        /// makes sure that all drink options are on the menu
+        /// </summary>
+        [Fact]
+        public void MenuReturnsAllDrinks()
+        {
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Aretino Apple Juice"); }); 
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Candlehearth Coffee"); }); 
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Decaf Candlehearth Coffee"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Warrior Water"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Blackberry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Cherry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Grapefruit Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Lemon Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Peach Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Small Watermelon Sailor Soda"); });
+            
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Aretino Apple Juice"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Candlehearth Coffee"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Decaf Candlehearth Coffee"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Warrior Water"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Blackberry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Cherry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Grapefruit Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Lemon Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Peach Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Medium Watermelon Sailor Soda"); });
+
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Aretino Apple Juice"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Candlehearth Coffee"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Decaf Candlehearth Coffee"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Warrior Water"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Blackberry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Cherry Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Grapefruit Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Lemon Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Peach Sailor Soda"); });
+            Assert.Contains(Menu.Drinks(), (item) => { return item.ToString().Equals("Large Watermelon Sailor Soda"); });
+            
+        }
+
+        []
+
+    }
+}
