c3b7ea09603322e8f12fde5132fe083d4b0417b3
Mason Potter <masonpotter1@ksu.edu>
2020-11-08 21:04:27 -0600

    test

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,6 +12,7 @@ namespace Website.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<IOrderItem> Itmes { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
